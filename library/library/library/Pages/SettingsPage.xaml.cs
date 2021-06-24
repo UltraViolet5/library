a3722cdf2c1e02444dbd8d50f5f4e0413daefce0
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2021-06-24 20:21:14 +0200

    Implement logout functionalitis.

--- library/library/library/Pages/SettingsPage.xaml.cs
+++ library/library/library/Pages/SettingsPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using library.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,13 @@ namespace library.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
+        private readonly SettingsViewModel _settingsViewModel;
         public SettingsPage()
         {
             InitializeComponent();
+
+            _settingsViewModel = new SettingsViewModel();
+            BindingContext = _settingsViewModel;
         }
     }
 }
---
+++ library/library/library/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using library.Pages;
+using Xamarin.Forms;
+
+namespace library.ViewModel
+{
+    class SettingsViewModel
+    {
+        public ICommand LogoutCommand { get; set; }
+
+        public SettingsViewModel()
+        {
+            LogoutCommand = new Command(LogoutExecute);
+        }
+
+        private void LogoutExecute()
+        {
+            Application.Current.Properties["IsLoggedIn"] = false;
+            App.Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
